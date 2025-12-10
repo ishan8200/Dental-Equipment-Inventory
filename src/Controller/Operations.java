3afd374ef3822f9298e74deca1ffa35f316f14ee
ishan8200 <surab820@gmail.com>
2025-12-10 19:32:48 +0545

    init

---
+++ src/Controller/Operations.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+import Model.InventoryModel;
+
+
+/**
+ *
+ * @author Ripple Device
+ */
+public class Operations {
+    
+}
